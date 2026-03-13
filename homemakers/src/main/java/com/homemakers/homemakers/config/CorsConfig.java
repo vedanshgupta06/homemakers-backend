73df98f4823c8531b4ebf5d874a4dc26db6ad10b
Vedansh <vedanshgupta06@gmail.com>
2026-03-14 00:07:55 +0530

    added one more server to prevent cors related error

--- homemakers/src/main/java/com/homemakers/homemakers/config/CorsConfig.java
+++ homemakers/src/main/java/com/homemakers/homemakers/config/CorsConfig.java
@@ -16,7 +16,10 @@ public class CorsConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://localhost:5174"
+        ));
         config.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "DELETE", "OPTIONS"
         ));
