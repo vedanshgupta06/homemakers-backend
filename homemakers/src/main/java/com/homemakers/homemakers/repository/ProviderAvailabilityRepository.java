31f7810a7dd97187ab682d578955e9d343ba206e
Vedansh <vedanshgupta06@gmail.com>
2026-04-01 23:59:14 +0530

    updated provider availability repo

--- homemakers/src/main/java/com/homemakers/homemakers/repository/ProviderAvailabilityRepository.java
+++ homemakers/src/main/java/com/homemakers/homemakers/repository/ProviderAvailabilityRepository.java
@@ -2,15 +2,19 @@ package com.homemakers.homemakers.repository;
 
 import com.homemakers.homemakers.model.Provider;
 import com.homemakers.homemakers.model.ProviderAvailability;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface ProviderAvailabilityRepository
         extends JpaRepository<ProviderAvailability, Long> {
-
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<ProviderAvailability> findWithLockById(Long id);
     List<ProviderAvailability> findByProviderAndDateAndActiveTrue(
             Provider provider,
             LocalDate date
@@ -23,8 +27,13 @@ public interface ProviderAvailabilityRepository
             LocalTime startTime
     );
 
+
+    List<ProviderAvailability> findByDateAndActiveTrue(LocalDate date);
+
     List<ProviderAvailability> findByProvider_Id(Long providerId);
 
     boolean existsByProviderAndActiveTrue(Provider provider);
+
+    List<ProviderAvailability> findByProvider(Provider provider);
 
 }
