246f87188c4f410dba05b05aa782030965f7018c
Vedansh <vedanshgupta06@gmail.com>
2026-05-02 22:38:02 +0530

    added user profile dto

---
+++ homemakers/src/main/java/com/homemakers/homemakers/dto/UserProfileDto.java
@@ -0,0 +1,96 @@
+package com.homemakers.homemakers.dto;
+
+public class UserProfileDto {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String city;
+    private String phone;
+    private String address;
+    private String role;
+    private String pincode;
+    private Double latitude;
+    private Double longitude;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    // getters & setters
+}
