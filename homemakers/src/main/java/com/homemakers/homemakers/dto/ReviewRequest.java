048ddd26701b6c4b40e80bf511870d46d1abb048
Vedansh <vedanshgupta06@gmail.com>
2026-05-04 20:56:48 +0530

    updated review request

--- homemakers/src/main/java/com/homemakers/homemakers/dto/ReviewRequest.java
+++ homemakers/src/main/java/com/homemakers/homemakers/dto/ReviewRequest.java
@@ -5,11 +5,9 @@ public class ReviewRequest {
     private int rating;
     private String comment;
 
-    public int getRating() {
-        return rating;
-    }
+    public int getRating() { return rating; }
+    public void setRating(int rating) { this.rating = rating; }
 
-    public String getComment() {
-        return comment;
-    }
+    public String getComment() { return comment; }
+    public void setComment(String comment) { this.comment = comment; }
 }
