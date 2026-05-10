8ae695338893e5a077d0806e1d0c031c2c54956c
himanshhs <himanshusinha323@gmail.com>
2026-05-10 16:00:16 +0530

    added service preview

---
+++ homemakers/src/main/java/com/homemakers/homemakers/dto/ServicePreviewResponseDTO.java
@@ -0,0 +1,11 @@
+package com.homemakers.homemakers.dto;
+
+public class ServicePreviewResponseDTO {
+
+    private String serviceName;
+
+    private int dailyMinutes;
+
+    private double monthlyPrice;
+
+}
