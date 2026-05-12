dd6f3a93ed9ae87a752cf5141332ae1455267695
vedanshgupta06 <vedanshgupta06@gmail.com>
2026-05-12 13:00:10 +0530

    fix render upload static paths

--- homemakers/src/main/java/com/homemakers/homemakers/config/FileUploadConfig.java
+++ homemakers/src/main/java/com/homemakers/homemakers/config/FileUploadConfig.java
@@ -4,16 +4,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.file.Paths;
+
 @Configuration
 public class FileUploadConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
+        String providerPath = Paths.get("uploads/providers")
+                .toFile()
+                .getAbsolutePath();
+
+        String documentPath = Paths.get("uploads/provider-documents")
+                .toFile()
+                .getAbsolutePath();
+
         registry.addResourceHandler("/providers/**")
-                .addResourceLocations("file:uploads/providers/");
+                .addResourceLocations("file:" + providerPath + "/");
 
         registry.addResourceHandler("/provider-documents/**")
-                .addResourceLocations("file:uploads/provider-documents/");
+                .addResourceLocations("file:" + documentPath + "/");
     }
 }
